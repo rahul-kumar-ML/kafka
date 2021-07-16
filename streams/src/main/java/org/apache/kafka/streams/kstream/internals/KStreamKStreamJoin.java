f2286c7563d2fed85a8dc1e7602e8a6b8b8e51dd
Matthias J. Sax <matthias@confluent.io>
2021-07-16 13:30:19 -0700

    HOTFIX: Init stream-stream left/outer join emit interval correctly (#11055)
    Follow up to #10917
    
    The fix from #10917 intended to reduce the emit frequency to save the creation cost of RocksDB iterators. However, we incorrectly initialized the "timer" with timestamp zero, and thus, the timer was always in the past and we did try to emit left/outer join result too often.
    
    This PR fixes the initialization of the emit interval timer to current wall-clock time to effectively 'enable' the fix from #10917.
    
    Reviewers: Sergio Peña <sergio@confluent.io>, Guozhang Wang <guozhang@confluent.io>
    (cherry picked from commit 3e38038278e8c6bb9c59432bedc81a06bc10d1de)

--- streams/src/main/java/org/apache/kafka/streams/kstream/internals/KStreamKStreamJoin.java
+++ streams/src/main/java/org/apache/kafka/streams/kstream/internals/KStreamKStreamJoin.java
@@ -103,7 +103,6 @@ class KStreamKStreamJoin<K, R, V1, V2> implements org.apache.kafka.streams.proce
                     true
                 )) {
                 outerJoinWindowStore = outerJoinWindowName.map(context::getStateStore);
-                sharedTimeTracker.nextTimeToEmit = context.currentSystemTimeMs();
 
                 sharedTimeTracker.setEmitInterval(
                     StreamsConfig.InternalConfig.getLong(
@@ -207,6 +206,9 @@ class KStreamKStreamJoin<K, R, V1, V2> implements org.apache.kafka.streams.proce
             // as throttling is a non-functional performance optimization
             if (context.currentSystemTimeMs() < sharedTimeTracker.nextTimeToEmit) {
                 return;
+            }
+            if (sharedTimeTracker.nextTimeToEmit == 0) {
+                sharedTimeTracker.nextTimeToEmit = context.currentSystemTimeMs();
             }
             sharedTimeTracker.advanceNextTimeToEmit();
 
