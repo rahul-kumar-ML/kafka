6f1e117cfc74594bf5c7ca037f254a83f28a3a9a
Confluent Jenkins <jenkins@confluent.io>
2023-02-26 06:53:48 +0000

    Create stabilization branch for cc-docker-kafka.18

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -119,7 +119,7 @@ def get_version(node=None):
         return DEV_BRANCH
 
 DEV_BRANCH = KafkaVersion("dev")
-DEV_VERSION = KafkaVersion("7.5.0-92")
+DEV_VERSION = KafkaVersion("7.5.0-cc")
 
 LATEST_METADATA_VERSION = "3.3"
 
