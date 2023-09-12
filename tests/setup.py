b590151981c1a2e3eb1266f2567d13c5cab66a73
Aneesh Garg <angarg@confluent.io>
2023-09-12 10:22:24 +0530

    Test: Trying to fix the breaking build due to requests v2.31.0

--- tests/setup.py
+++ tests/setup.py
@@ -51,7 +51,7 @@ setup(name="kafkatest",
       license="apache2.0",
       packages=find_packages(),
       include_package_data=True,
-      install_requires=["ducktape<0.9", "requests==2.31.0"],
+      install_requires=["ducktape @ git+https://git@github.com/confluentinc/ducktape.git@libFix8", "requests==2.31.0"],
       tests_require=["pytest", "mock"],
       cmdclass={'test': PyTest},
       zip_safe=False
